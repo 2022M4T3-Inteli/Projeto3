1450f187d462d559d25d5ca849757f90c0fe8aae
mflorencio-inteli <marcos.silva@sou.inteli.edu.br>
2022-11-13 15:37:39 -0300

    server setup

---
+++ src/Backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const tagsRouter = require("./routers/tagsRouter");
+const morgan = require("morgan");
+////////////////////////////////////////////////////////////////////////
+
+const app = express();
+
+// middlewares
+app.use(express.json()); // adds body to request
+if (process.env.NODE_ENV == "development") app.use(morgan("dev")); // http requests logger
+
+// routers
+app.use("/api/v1/tags", tagsRouter);
+
+module.exports = app;
