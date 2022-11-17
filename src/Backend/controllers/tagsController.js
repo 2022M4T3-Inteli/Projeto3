2b344669c95adf1d86f8db2775c7a229b434a2d0
mflorencio-inteli <marcos.silva@sou.inteli.edu.br>
2022-11-17 18:45:17 -0300

    change hasMoved to is Moving

--- src/Backend/controllers/tagsController.js
+++ src/Backend/controllers/tagsController.js
@@ -7,11 +7,11 @@ let isTagActivated;
 exports.tagUpdate = function (req, res) {
   if (!tagReading)
     tagReading = {
-      macAddr: req.body.macAddr,
-      hasMoved: req.body.hasMoved,
+      macAddr: req.body.macAddress,
+      isMoving: req.body.isMoving,
     };
   tagReading.positions = req.body.positions;
-  !tagReading.hasMoved && (tagReading.hasMoved = req.body.hasMoved);
+  tagReading.isMoving = req.body.isMoving;
 
   if (!isTagActivated)
     isTagActivated = {
@@ -29,14 +29,13 @@ exports.getTagStatus = function (req, res) {
     .status(200)
     .json({
       macAddr: tagReading.macAddr,
-      hasMoved: tagReading.hasMoved,
+      isMoving: tagReading.isMoving,
     })
     .end();
 };
 
 exports.postTagStatus = function (req, res) {
   isTagActivated.activated = req.body.activated;
-  !req.body.activated && (tagReading.hasMoved = false);
 
   console.log(isTagActivated);
 
