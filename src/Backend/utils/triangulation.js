5d4a2e064e591f155494ce7e9b418c197de359d5
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2022-12-13 14:39:17 -0300

    Update triangulation.js

--- src/Backend/utils/triangulation.js
+++ src/Backend/utils/triangulation.js
@@ -26,45 +26,40 @@ class Triangulation {
   pointsList = [];
   distancesList = [0, 0, 0];
 
-  yPointA1toA2() {
+  yPointA1toA2() {              // Formula que calcula a coordenada y do Ponto B usando apenas A1 e A2
     let dA1to2 = Math.pow(this.distancesList[0], 2);
     let dA2to2 = Math.pow(this.distancesList[1], 2);
     let y2to2 = Math.pow(this.pointsList[1].getY(), 2);
-    let y2toX2 = Math.pow(this.pointsList[0].getY(), 2);
-
+    let y2toX2 = 2 * (this.pointsList[1].getY());
     if (y2toX2 == 0) {
       y2toX2 = 1;
     }
-
     let yb = (dA1to2 - dA2to2 + y2to2) / y2toX2;
-    return yb;
-  }
-
-  xPointA1toA2() {
+    return (yb);
+  };
+  xPointA1toA2() {  // Formula que calcula a coordenada x do Ponto B usando apenas A1 e A2
     let dA1to2 = Math.pow(this.distancesList[0], 2);
     let yb = this.yPointA1toA2();
     let xb = Math.sqrt(Math.abs(dA1to2 - yb));
-    return xb;
-  }
-
-  yPointA1toA3() {
+    return (xb);
+  };
+  yPointA1toA3() {  // Formula que calcula a coordenada x do Ponto B usando apenas A1 e A3
     let dA1to2 = Math.pow(this.distancesList[0], 2);
     let dA2to2 = Math.pow(this.distancesList[2], 2);
     let y2to2 = Math.pow(this.pointsList[2].getX(), 2);
-    let y2toX2 = 2 * this.pointsList[2].getX();
+    let y2toX2 = 2 * (this.pointsList[2].getX());
     if (y2toX2 == 0) {
       y2toX2 = 1;
     }
     let yb = (dA1to2 - dA2to2 + y2to2) / y2toX2;
-    return yb;
-  }
-
-  xPointA1toA3() {
-    let dA1to2 = Math.pow(this.pointsList[0], 2);
+    return (yb);
+  };
+  xPointA1toA3() {  // Formula que calcula a coordenada y do Ponto B usando apenas A1 e A3
+    let dA1to2 = Math.pow(this.distancesList[0], 2);
     let yb = this.yPointA1toA3();
     let xb = Math.sqrt(Math.abs(dA1to2 - yb));
-    return xb;
-  }
+    return (xb);
+  };
 
   putPoint(nr, x, y) {
     const _point = new Point();
